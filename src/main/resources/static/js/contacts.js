6e75cf7ae361adc0c7653ee77b950968d7743bf1
atul-patel-silver <atulpatel2001a@gmail.com>
2024-09-24 17:26:55 +0530

    commit

--- src/main/resources/static/js/contacts.js
+++ src/main/resources/static/js/contacts.js
@@ -1,6 +1,6 @@
 const viewContactModal=document.getElementById("view_contact_modal")
 // const baseUrl='http://localhost:8081';
-const baseUrl='http://scm-2-0-v1.onrender.com';
+const baseUrl='https://scm-2-0-v1.onrender.com';
 
 const options = {
     placement: 'bottom-right',
